ff262306abcc6c9a24d51201171982556576e4ed
nipun-thejan <thejanfonseka@gmail.com>
2023-02-10 12:41:46 +0530

    init

---
+++ backend/lms/src/main/java/com/thejan/lms/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.thejan.lms.controller;
+
+
+import com.thejan.lms.entity.User;
+import com.thejan.lms.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    UserService userService;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<String> findById(@PathVariable Long id) {
+		return new ResponseEntity<>(userService.getUser(id).getUsername(), HttpStatus.OK);
+	}
+
+    @PostMapping("/register")
+	public ResponseEntity<HttpStatus> createUser(@Valid @RequestBody User user) {
+		userService.saveUser(user);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+}
